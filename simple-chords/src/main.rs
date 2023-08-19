9c8e59de7ca085a24cedc92177e65b36af717f61
Lasse Becker <lasse@cpsh.de>
2023-08-19 13:51:46 +0200

    parse altering chords and lyrics

--- simple-chords/src/main.rs
+++ simple-chords/src/main.rs
@@ -3,8 +3,14 @@ use std::fs::File;
 use std::io::{self, Write };
 mod parsingstate;
 
+#[derive(Clone, Debug)]  
+enum AstElement {
+    Heading(String),
+    Chord(ChordToken),
+    LyricLine(String)
+}   
 
-type ParsingState = parsingstate::ParsingState<ChordToken>;
+type ParsingState = parsingstate::ParsingState<AstElement>;
 
 #[derive(Clone, Debug)]  
 struct ChordToken {
@@ -13,23 +19,30 @@ struct ChordToken {
     start_char_index: usize
 }
 
-#[derive(Clone,Debug)]
-struct HeadingToken {
-    str: String
-}
 
 
-fn parse_heading(state: &mut ParsingState) -> HeadingToken {
+fn parse_heading(state: &mut ParsingState) {
     state.skip_whitespace();
     let c = state.step_one_forward();
     if c != Some('#') {
         panic!("Syntax Error: Expected the first non whitespace character to be '#' ")
     }
-    let title = state.read_line();
-    return HeadingToken {
-        str: title.unwrap_or(String::new()).trim().to_string()
-    }
+    let title = state.read_line().unwrap_or(String::new()).trim().to_string();
+    state.push_to_result(AstElement::Heading(title))
 }
+
+fn parse_till_song_start(state: &mut ParsingState){
+    state.skip_whitespace();
+    let c0 = state.step_one_forward();
+    let c1 = state.step_one_forward();
+    let c2 = state.step_one_forward();
+    let c3 = state.step_one_forward();
+    if c0 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) after heading.")};
+    if c1 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) after heading.")};
+    if c2 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) after heading.")};
+    if c3 != Some('\n') { panic!("Syntax Error: new line after ``` (three tics).")};
+}
+
 
 
 fn parse_line_of_chords(state: &mut ParsingState) {
@@ -48,7 +61,7 @@ fn parse_line_of_chords(state: &mut ParsingState) {
                 })
             }
             (Some(chord), ' ') | (Some(chord), '\n') => {
-                state.push_to_result(chord);
+                state.push_to_result(AstElement::Chord(chord));
                 current_chord = None;
             },
             (Some(chord), c) => {
@@ -65,17 +78,11 @@ fn parse_line_of_chords(state: &mut ParsingState) {
     };
 }
 
-fn parse_till_song_start(state: &mut ParsingState){
-    state.skip_whitespace();
-    let c0 = state.step_one_forward();
-    let c1 = state.step_one_forward();
-    let c2 = state.step_one_forward();
-    let c3 = state.step_one_forward();
-    if c0 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) after heading.")};
-    if c1 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) after heading.")};
-    if c2 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) after heading.")};
-    if c3 != Some('\n') { panic!("Syntax Error: new line after ``` (three tics).")};
+fn parse_lyrics_line(state: &mut ParsingState){
+    let lyric = state.read_line().unwrap_or(String::new());
+    state.push_to_result(AstElement::LyricLine(lyric))
 }
+
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -92,12 +99,13 @@ fn main() -> io::Result<()> {
     let heading = parse_heading(state);
 
     parse_till_song_start(state);
-
+ 
     while !state.is_done() {
         parse_line_of_chords(state);
+        parse_lyrics_line(state);
     }
 
-    let song_output = state.result.iter().map(|chord| {format!("{:?}\n", chord)}).collect::<Vec<_>>().join("");
+    let song_output = state.result.iter().map(|el| {format!("{:?}\n", el)}).collect::<Vec<_>>().join("");
     let mut output = format!("{:?}",heading);
     output.push('\n');
     output.push_str(&song_output);
@@ -114,3 +122,4 @@ fn main() -> io::Result<()> {
 
     Ok(())
 }
+    
