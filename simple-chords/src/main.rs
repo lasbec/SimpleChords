4c49c350836b29cd17ecb17b4989aa8f99c75a21
Lasse Becker <lasse@cpsh.de>
2023-08-19 21:04:05 +0200

    introduced ast

--- simple-chords/src/main.rs
+++ simple-chords/src/main.rs
@@ -7,14 +7,14 @@ mod parsingstate;
 mod music;
 
 #[derive(Clone, Debug)]  
-enum AstElement {
+enum Token {
     Heading(String),
     Markup(String),
-    Chord(ChordToken),
+    ChordLine(Vec<ChordToken>),
     LyricLine(String)
 }   
 
-type ParsingState = parsingstate::ParsingState<AstElement>;
+type ParsingState = parsingstate::ParsingState<Token>;
 
 #[derive(Clone, Debug)]  
 struct ChordToken {
@@ -32,7 +32,7 @@ fn parse_heading(state: &mut ParsingState) {
         panic!("Syntax Error: Expected the first non whitespace character to be '#' ")
     }
     let title = state.read_line().trim().to_string();
-    state.push_to_result(AstElement::Heading(title))
+    state.push_to_result(Token::Heading(title))
 }
 
 fn parse_till_song_start(state: &mut ParsingState){
@@ -49,7 +49,16 @@ fn parse_till_song_start(state: &mut ParsingState){
 
 
 fn parse_line(state: &mut ParsingState) {
-    if state.peek() == Some('[') {
+    if state.peek() == Some('`') {
+        let c0 = state.read_next();
+        let c1 = state.read_next();
+        let c2 = state.read_next();
+        let c3 = state.read_next();
+        if c0 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) to end the song.")};
+        if c1 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) to end the song.")};
+        if c2 != Some('`')  { panic!("Syntax Error: expected ``` (three tics) to end the song.")};
+        if c3 != Some('\n') { panic!("Syntax Error: new line expected after ``` (three tics).")};
+    } else if state.peek() == Some('[') {
          parse_markup_line(state); 
     } else {
         let chords_result = parse_line_of_chords(state);
@@ -58,12 +67,10 @@ fn parse_line(state: &mut ParsingState) {
                 let rest_line = state.read_line();
                 let mut res = s.clone();
                 res.push_str(&rest_line);
-                state.push_to_result(AstElement::LyricLine(res));
+                state.push_to_result(Token::LyricLine(res));
             },
             ChordsLineParsingResult::Success(v) =>{
-                for r in v {
-                    state.push_to_result(r);
-                }
+                state.push_to_result(Token::ChordLine(v));
             }
         }
     }
@@ -73,12 +80,12 @@ fn parse_line(state: &mut ParsingState) {
 fn parse_markup_line(state: &mut ParsingState) {
     state.read_next(); // shoud be '['
     let result = state.read_till(|c| c == ']');
-    state.push_to_result(AstElement::Markup(result));
+    state.push_to_result(Token::Markup(result));
 }
 
 #[derive(Clone, Debug)]
 enum ChordsLineParsingResult {
-    Success(Vec<AstElement>),
+    Success(Vec<ChordToken>),
     Consumed(String)
 }
 
@@ -101,11 +108,11 @@ fn parse_line_of_chords(state: &mut ParsingState) -> ChordsLineParsingResult {
 
         let chord_opt = Chord::from_string(&candidate);
         if let Some(chord) = chord_opt {
-            result.push(AstElement::Chord(ChordToken {
+            result.push(ChordToken {
                 str:chord,
                 start_char_index,
                 start_line_index,
-            }));
+            });
         } else {
             return ChordsLineParsingResult::Consumed(consumed);
         }
@@ -114,6 +121,109 @@ fn parse_line_of_chords(state: &mut ParsingState) -> ChordsLineParsingResult {
 
 }
 
+
+#[derive(Clone, Debug)]
+struct AST {
+    heading: String,
+    song: Vec<SongSection>
+}
+
+#[derive(Clone, Debug)]
+struct SongSection {
+    markup: String,
+    lines: Vec<SongLine>
+}
+
+type SongLine = Vec<SongLineChar>;
+
+
+#[derive(Clone, Debug)]
+struct SongLineChar {
+    char: char,
+    chord: Option<Chord>
+}
+
+fn chords_to_song_line(chords: &Vec<ChordToken>) -> SongLine {
+    let mut result = Vec::new();
+    for chord in chords {
+        result.push(SongLineChar{
+            char: ' ',
+            chord: Some(chord.str.clone())
+        })
+    }
+    return result;
+}
+
+fn lyric_to_songline(lyrics:&String)->SongLine {
+    let mut result = Vec::new();
+    for c in lyrics.chars() {
+        result.push(SongLineChar { char: c, chord: None })
+    }
+    return result;
+}
+
+fn make_song_line(lyrics: &String, chords:&Vec<ChordToken>) -> SongLine {
+    let mut result = Vec::new();
+    let mut chords_iter = chords.iter();
+    let next_chord = chords_iter.next();
+    for (i,c) in lyrics.chars().enumerate() {
+        if let Some(nc) = next_chord {
+            if nc.start_char_index == i {
+                result.push(SongLineChar { char: c, chord: Some(nc.str.clone()) });
+                continue;
+            }
+        }
+        result.push(SongLineChar { char: c, chord: None })
+    }
+    return result;
+}
+
+
+
+
+fn build_ast(tokens: Vec<Token>) -> AST {
+    let mut token_iterator = tokens.iter();
+
+    let mut heading = String::new();
+    let mut sections: Vec<SongSection> = Vec::new();
+    let mut current_section = SongSection {
+        markup: String::new(),
+        lines: Vec::new()
+    };
+
+    let mut current_chords = None;
+    
+    while let Some(token) = token_iterator.next() {
+        match token {
+            Token::ChordLine(chords) => {
+                if let Some(cc) = current_chords {
+                    let value:SongLine = chords_to_song_line(cc);
+                    current_section.lines.push(value);
+                } 
+                current_chords = Some(chords);
+            },
+            Token::LyricLine(lyrics) => {
+                current_section.lines.push(lyric_to_songline(lyrics))
+            },
+            Token::Markup(section_name) => {
+                sections.push(current_section);
+                current_section = SongSection {
+                    markup: section_name.clone(),
+                    lines: Vec::new()
+                }
+            },
+            Token::Heading(h) => {
+                heading = h.clone();
+            }
+        }
+    }
+    sections.push(current_section);
+    return AST {
+        heading: heading.clone(),
+        song: sections
+    }
+
+}
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -134,13 +244,8 @@ fn main() -> io::Result<()> {
         parse_line(state);
     }
 
-    let song_output = state.result.iter().map(
-        |el| {format!("{:?}\n", el)}).collect::<Vec<_>>().join(""
-    );
-    let mut output = format!("{:?}",heading);
-    output.push('\n');
-    output.push_str(&song_output);
-
+    let ast = build_ast(state.result.clone());
+    let mut output = format!("{:?}",ast);
 
     // Open the output file
     let output_path = &args[2];
