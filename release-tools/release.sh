c477cb9e7b60b29aa3f75c4b2da4e6d45ab6b3f4
Lasse Becker <lasse@cpsh.de>
2023-09-13 21:42:17 +0200

    release script test

--- release-tools/release.sh
+++ release-tools/release.sh
@@ -1,1 +1,10 @@
 #! /bin bash
+$releaseType = 
+
+$newVersion = node ./update-version-number.js ($releaseType)
+
+git stash
+git checkout master
+git add .
+git commit -m "release $releaseType $newVersion"
+git push origin master --force
