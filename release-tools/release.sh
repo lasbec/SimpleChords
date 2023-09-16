fb74c76d757fd48e8f6912c83e87510165f1c3b9
Lasse Becker <lasse@cpsh.de>
2023-09-16 08:46:11 +0200

    fix package.json "main" option

--- release-tools/release.sh
+++ release-tools/release.sh
@@ -1,10 +1,19 @@
 #! /bin bash
-$releaseType = 
+$releaseType = $1
 
-$newVersion = node ./update-version-number.js ($releaseType)
+git.assertAuthenticated
+git.assertNoUncommitedChanges
+git.assertNoUnpushedChanges
+git.pull
+git.assertMasterIsCheckedOut
 
 git stash
 git checkout master
 git add .
 git commit -m "release $releaseType $newVersion"
 git push origin master --force
+
+$newVersion = node ./update-version-number.js $releaseType
+assertSuccessfulVersionUpcount
+
+npm release $releaseType
