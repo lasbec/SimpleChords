7b22bf687146f7523389e88a1bc716b10b46cf47
Lasse Becker <lasse@cpsh.de>
2023-10-31 18:20:47 +0100

    test HigherOrderBox: fixed size appanding child

--- src/Drawing/Boxes/HigherOrderBox.test.js
+++ src/Drawing/Boxes/HigherOrderBox.test.js
@@ -6,6 +6,9 @@ import { RectangleImpl } from "../Figures/RectangleImpl.js";
 import { PointImpl } from "../Figures/PointImpl.js";
 import { LEN } from "../../Shared/Length.js";
 import { expect } from "vitest";
+import { DebugBox } from "./DebugBox.js";
+import { FixedSizeBox } from "./FixedSizeBox.js";
+import exp from "constants";
 
 describe("HigherOrderBox", () => {
   it("Minimal width and hight ", () => {
@@ -18,5 +21,44 @@ describe("HigherOrderBox", () => {
       hob.rectangle.width.in("mm").toFixed(8),
       hob.rectangle.height.in("mm").toFixed(8),
     ]).toEqual(["60.00000000", "50.00000000"]);
+  });
+  it("two children; unbound", () => {
+    const leftBottom = new DebugBox(PointImpl.origin());
+    const rightTop = new DebugBox(
+      PointImpl.origin().moveUp(LEN(50, "mm")).moveRight(LEN(60, "mm"))
+    );
+    const hob = new HigherOrderBox(
+      [leftBottom, rightTop],
+      BoundsImpl.unbound()
+    );
+    expect([
+      hob.rectangle.width.in("mm").toFixed(8),
+      hob.rectangle.height.in("mm").toFixed(8),
+      hob.rectangle.getPoint("left", "bottom").x.in("mm").toFixed(8),
+      hob.rectangle.getPoint("left", "bottom").y.in("mm").toFixed(8),
+      hob.rectangle.getPoint("right", "top").x.in("mm").toFixed(8),
+      hob.rectangle.getPoint("right", "top").y.in("mm").toFixed(8),
+    ]).toEqual([
+      "63.00000000",
+      "53.00000000",
+      "-1.50000000",
+      "-1.50000000",
+      "61.50000000",
+      "51.50000000",
+    ]);
+  });
+
+  it("Fixed size", () => {
+    const page = FixedSizeBox.newPage({
+      width: LEN(210, "mm"),
+      height: LEN(297, "mm"),
+    });
+    const widthBefore = page.rectangle.width.in("mm").toFixed(8);
+    const heightBefore = page.rectangle.height.in("mm").toFixed(8);
+    page.appendChild(new DebugBox(PointImpl.origin()));
+    const widthAfter = page.rectangle.width.in("mm").toFixed(8);
+    const heightAfter = page.rectangle.height.in("mm").toFixed(8);
+
+    expect([widthBefore, heightBefore]).toEqual([widthAfter, heightAfter]);
   });
 });
